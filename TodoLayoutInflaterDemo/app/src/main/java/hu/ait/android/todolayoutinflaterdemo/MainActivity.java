02c71d7e1846d39a90ec36c797e406a3e6639be1
Péter Ekler <peter.ekler@gmail.com>
2017-10-09 15:29:41 +0200

    Login layout demo and ConstraintLayout, LayoutInflaterDemo with a mini todo app

---
+++ TodoLayoutInflaterDemo/app/src/main/java/hu/ait/android/todolayoutinflaterdemo/MainActivity.java
@@ -0,0 +1,52 @@
+package hu.ait.android.todolayoutinflaterdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.etTodo)
+    EditText etTodo;
+    @BindView(R.id.cbImportant)
+    CheckBox cbImportant;
+    @BindView(R.id.layoutContent)
+    LinearLayout layoutContent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnAdd)
+    public void addPressed(Button btnAdd) {
+        final View todoRow = getLayoutInflater().inflate(
+                R.layout.layout_todo_row,
+                null, false);
+
+        TextView tvTodo = (TextView) todoRow.findViewById(R.id.tvTodo);
+        tvTodo.setText(etTodo.getText().toString());
+
+        Button btnDelete = (Button) todoRow.findViewById(R.id.btnDelete);
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layoutContent.removeView(todoRow);
+            }
+        });
+
+        layoutContent.addView(todoRow);
+    }
+}
